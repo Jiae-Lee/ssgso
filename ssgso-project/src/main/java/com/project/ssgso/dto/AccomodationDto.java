7684b4de4eaab3e2b17bbc033b04802d66625df5
dsunni <dongsunlee00@gmail.com>
2020-06-26 10:00:55 +0900

    Update ssgso

--- ssgso-project/src/main/java/com/project/ssgso/dto/AccomodationDto.java
+++ ssgso-project/src/main/java/com/project/ssgso/dto/AccomodationDto.java
@@ -1,142 +1,127 @@
 package com.project.ssgso.dto;
 
 public class AccomodationDto {
-	private int ac_no;
-	private String name;
-	private String info;
-	private String ac_img;
-	private Double latitude;
-	private Double longitude;
-	private int ac_price;
-	private String category;
-	private String address;
-	private int mem_no;
-	
-	public AccomodationDto() {
-		
-	}
+   private int ac_no;
+   private String name;
+   private String info;
+   private String ac_img;
+   private Double latitude;
+   private Double longitude;
+   private String ac_phone;
+   private String ac_account;
+   private String ac_price;
+   private String mem_no;
+   private String category;
+   private String address;
+   private String id;
+   
+   public AccomodationDto() {
+      
+   }
 
-	public int getAc_no() {
-		return ac_no;
-	}
+   public int getAc_no() {
+      return ac_no;
+   }
 
-	public void setAc_no(int ac_no) {
-		this.ac_no = ac_no;
-	}
+   public void setAc_no(int ac_no) {
+      this.ac_no = ac_no;
+   }
 
-	public String getName() {
-		return name;
-	}
+   public String getName() {
+      return name;
+   }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+   public void setName(String name) {
+      this.name = name;
+   }
 
-	public String getInfo() {
-		return info;
-	}
+   public String getInfo() {
+      return info;
+   }
 
-	public void setInfo(String info) {
-		this.info = info;
-	}
+   public void setInfo(String info) {
+      this.info = info;
+   }
 
-	public String getAc_img() {
-		return ac_img;
-	}
+   public String getAc_img() {
+      return ac_img;
+   }
 
-	public void setAc_img(String ac_img) {
-		this.ac_img = ac_img;
-	}
-	
-	
-	public Double getLatitude() {
-		return latitude;
-	}
+   public void setAc_img(String ac_img) {
+      this.ac_img = ac_img;
+   }
+   
+   
+   public Double getLatitude() {
+      return latitude;
+   }
 
-	public void setLatitude(Double latitude) {
-		this.latitude = latitude;
-	}
+   public void setLatitude(Double latitude) {
+      this.latitude = latitude;
+   }
 
-	public Double getLongitude() {
-		return longitude;
-	}
+   public Double getLongitude() {
+      return longitude;
+   }
 
-	public void setLongitude(Double longitude) {
-		this.longitude = longitude;
-	}
+   public void setLongitude(Double longitude) {
+      this.longitude = longitude;
+   }
 
-	public String getAc_account() {
-		return ac_account;
-	}
+   public String getAc_account() {
+      return ac_account;
+   }
 
-	public void setAc_account(String ac_account) {
-		this.ac_account = ac_account;
-	}
+   public void setAc_account(String ac_account) {
+      this.ac_account = ac_account;
+   }
 
-	public String getAc_phone() {
-		return ac_phone;
-	}
+   public String getAc_phone() {
+      return ac_phone;
+   }
 
-	public void setAc_phone(String ac_phone) {
-		this.ac_phone = ac_phone;
-	}
+   public void setAc_phone(String ac_phone) {
+      this.ac_phone = ac_phone;
+   }
 
-	public String getAc_price() {
-		return ac_price;
-	}
+   public String getAc_price() {
+      return ac_price;
+   }
 
-	public void setAc_price(String ac_price) {
-		this.ac_price = ac_price;
-	}
+   public void setAc_price(String ac_price) {
+      this.ac_price = ac_price;
+   }
 
-	public String getMem_no() {
-		return mem_no;
-	}
+   public String getMem_no() {
+      return mem_no;
+   }
 
-	public void setMem_no(String mem_no) {
-		this.mem_no = mem_no;
-	}
+   public void setMem_no(String mem_no) {
+      this.mem_no = mem_no;
+   }
 
-	public String getCategory() {
-		return category;
-	}
+   public String getCategory() {
+      return category;
+   }
 
-	public void setCategory(String category) {
-		this.category = category;
-	}
+   public void setCategory(String category) {
+      this.category = category;
+   }
 
-	public String getAddress() {
-		return address;
-	}
+   public String getAddress() {
+      return address;
+   }
 
-	public void setAddress(String address) {
-		this.address = address;
-	}
+   public void setAddress(String address) {
+      this.address = address;
+   }
 
-	public int getMem_no() {
-		return mem_no;
-	}
+   public String getId() {
+      return id;
+   }
 
-	public void setMem_no(int mem_no) {
-		this.mem_no = mem_no;
-	}
-
-	public int getAc_price() {
-		return ac_price;
-	}
-
-	public void setAc_price(int ac_price) {
-		this.ac_price = ac_price;
-	}
-
-	public String getCategory() {
-		return category;
-	}
-
-	public void setCategory(String category) {
-		this.category = category;
-	}
-
-	
+   public void setId(String id) {
+      this.id = id;
+   }
 
 }
